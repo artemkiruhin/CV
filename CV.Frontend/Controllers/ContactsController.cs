c4c1c668b6345cddcb4d8fb55384255de383d273
Artemka <artem110805@mail.ru>
2023-10-30 11:04:59 +0300

    Feedback to database created

--- CV.Frontend/Controllers/ContactsController.cs
+++ CV.Frontend/Controllers/ContactsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CV.Frontend.ViewModels;
+using CV.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CV.Frontend.Controllers {
     public class ContactsController : Controller {
@@ -8,6 +10,46 @@ namespace CV.Frontend.Controllers {
 
         public IActionResult Success() {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult OnFeedback(FeedbackViewModel feedbackVM) {
+            if (feedbackVM == null)
+            {
+                
+            }
+            else
+            {
+                var feedbackModel = new CV.Models.Feedback()
+                {
+                    Title = feedbackVM.Title,
+                    Message = feedbackVM.Message,
+                    Sender = feedbackVM.Mail,
+                    Created = DateTime.Now
+                };
+
+                var feedbackDbModel = new CV.DatabaseModels.Feedback()
+                {
+                    Title = feedbackModel.Title,
+                    Message = feedbackModel.Message,
+                    Sender = feedbackModel.Sender,
+                    Created = DateTime.Now
+                };
+
+                var client = new HttpClient();
+
+                var context = new RouteData();
+
+                var controller = new CV.API.Controllers.FeedbackController();
+
+                controller.AddFeedback(feedbackModel);
+
+                Console.WriteLine(feedbackDbModel.Id);
+
+                return RedirectToAction(controllerName: "Contacts", actionName: "Success");
+            }
+
+            return RedirectToAction(controllerName: "Home", actionName: "Index");
         }
     }
 }
---
+++ CV.Frontend/ViewModels/FeedbackViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CV.Frontend.ViewModels {
+    public class FeedbackViewModel {
+        [Required(ErrorMessage = "Пожалуйста, введите тему сообщения")]
+        public string Title { get; set; }
+
+
+        [Required(ErrorMessage = "Пожалуйста, введите сообщение")]
+        public string Message { get; set; }
+
+
+        [Required(ErrorMessage = "Пожалуйста, введите ваши контакты")]
+        public string Mail { get; set; }
+    }
+}
